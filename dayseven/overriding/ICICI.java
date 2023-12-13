5f999b5f61780bc5493ea7c54564c18c8484a42b
Basavaraj S Patil <103230855+PatilBasavaraj2000@users.noreply.github.com>
2023-12-13 16:09:15 +0530

    First Commit

---
+++ dayseven/overriding/ICICI.java
@@ -0,0 +1,10 @@
+package dayseven.overriding;
+//sub class
+public class ICICI extends  RBI {
+
+	@Override
+	public float getRateOfInterest()
+	{
+		return 9.0f;
+	}
+}
---
+++ dayseven/overriding/RBI.java
@@ -0,0 +1,9 @@
+package dayseven.overriding;
+
+//super class
+public class RBI {
+	public float getRateOfInterest() {
+		return 6.8f;
+	}
+
+}

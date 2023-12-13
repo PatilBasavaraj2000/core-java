6a8c35de606745e13a7b8fc0bb3fb7486cc70b25
Basavaraj S Patil <103230855+PatilBasavaraj2000@users.noreply.github.com>
2023-12-13 16:09:50 +0530

    First Commit

---
+++ dayeight/impinterfaces/Bank.java
@@ -0,0 +1,15 @@
+package dayeight.impinterfaces;
+
+//Bank Interface
+public interface Bank {
+	
+	double MIN_BALANCE = 1000;
+	double DEPOSIT_LIMIT = 25000;
+	double INT_RATE = 8.7;
+	
+	void deposit(Account account,double amount);
+	void withdraw(Account account,double amount);
+	
+	
+
+}

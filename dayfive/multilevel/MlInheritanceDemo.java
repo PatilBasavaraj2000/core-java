d149aa0fd99f353b183ce647be6f2c1b86c33a28
Basavaraj S Patil <103230855+PatilBasavaraj2000@users.noreply.github.com>
2023-12-13 16:07:44 +0530

    First Commit

---
+++ dayfive/multilevel/MlInheritanceDemo.java
@@ -0,0 +1,21 @@
+package dayfive.multilevel;
+
+public class MlInheritanceDemo {
+
+	public static void main(String[] args) {
+		
+		
+		City city = new City ();
+		city.setCityName("bangalore");
+		city.setArea(267.78f);
+		city.setStateName("karnataka");
+		city.setLanguage("kannada");
+		city.setCountryName("India");
+		city.setCapital("Delhi");
+		
+		System.out.println(city);
+		
+
+	}
+
+}

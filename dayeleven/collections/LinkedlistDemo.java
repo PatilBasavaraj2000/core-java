203c4e04b3adaa91f77f895ddf221481d4c45e43
Basavaraj S Patil <103230855+PatilBasavaraj2000@users.noreply.github.com>
2023-12-17 22:33:38 +0530

    First Commit

---
+++ dayeleven/collections/LinkedlistDemo.java
@@ -0,0 +1,25 @@
+package dayeleven.collections;
+
+
+import java.util.Iterator;
+import java.util.LinkedList;
+
+public class LinkedlistDemo {
+
+	public static void main(String[] args) {
+
+      LinkedList <String> ll = new LinkedList<String>();
+      
+      ll.add("Hello");
+      ll.add("TNSIF");
+      ll.add("java");
+      
+      Iterator <String> itt = ll.iterator();
+      while(itt.hasNext()) {
+    	  System.out.println(itt.next());
+      }
+      
+
+	}
+
+}
---
+++ dayeleven/collections/VectorDemo.java
@@ -0,0 +1,25 @@
+package dayeleven.collections;
+
+import java.util.Iterator;
+import java.util.Vector;
+
+public class VectorDemo {
+
+	public static void main(String[] args) {
+		
+		Vector <String> vv = new Vector<String>();
+		
+		vv.add("Good");
+		vv.add("evening");
+		vv.add("all");
+		
+		Iterator <String> itr = vv.iterator();
+		
+		while(itr.hasNext()) {
+			System.out.println(itr.next());
+		}
+
+
+	}
+
+}
